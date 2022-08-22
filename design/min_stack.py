d4929a29b6e1c04db46342471e1c6c83f6a9d44b
Mikhail Nikolaev <34393347+mi-doc@users.noreply.github.com>
2022-08-22 20:17:20 +0000

    Minimum stack

---
+++ design/min_stack.py
@@ -0,0 +1,45 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = float('inf')
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.min = min(self.min, val)
+
+    def pop(self) -> None:
+        if (val := self.stack.pop()) == self.min:
+            if self.stack:
+                self.min = min(self.stack)
+            else:
+                self.min = float('inf')
+        return val
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
+        
+
+if __name__ == "__main__":
+    obj = MinStack()
+
+    obj.push(55)
+    obj.push(32)
+    print(obj.pop())
+    print(obj.top())
+    obj.push(12)
+    obj.push(24)
+    print(obj.getMin())
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
