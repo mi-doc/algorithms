3588dd7b203723c68d8daf29606764ce85b7767d
Mikhail Nikolaev <34393347+mi-doc@users.noreply.github.com>
2022-08-27 21:36:29 +0300

    Scripts updated

--- change_zsh_theme.sh
+++ change_zsh_theme.sh
@@ -2,9 +2,5 @@
 
 read -p "Enter zsh theme: " theme
 
-grep '^ZSH_THEME=' ~/.zshrc
-sed -i "s/^\(ZSH_THEME=\"\).*/\1$theme\"/" ~/.zshrc
-grep '^ZSH_THEME=' ~/.zshrc
-
-zsh
-source ~/.zshrc
+echo $theme
+sed -i "s/^\(ZSH_THEME=\"\).*/\1"$theme"\"/" text.txt
