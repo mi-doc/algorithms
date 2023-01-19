49878f965ab958eae5db36cf70de76668d5d25b2
Mikhail Nikolaev <34393347+mi-doc@users.noreply.github.com>
2023-01-19 16:06:36 +0000

    Subsets task updated

--- backtracking/subsets.py
+++ backtracking/subsets.py
@@ -10,19 +10,37 @@ Return the solution in any order.
 
 class Solution:
     """
-    My solution!
+    My and leetcode solutions
     """
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        """ My solution """
         res = set()
         self.dfs(nums, [], res)
         return list(list(r) for r in res)
     
     def dfs(self, nums, path, res):
         res.add(tuple(sorted(path)))
-        if not nums:
-            return res
-        
-        for i, n in enumerate(nums):
-            # res.add(tuple(path))
-            self.dfs(nums[:i]+nums[i+1:], path+[n], res)
+        if nums:
+            for i, n in enumerate(nums):
+                self.dfs(nums[:i]+nums[i+1:], path+[n], res)
             
+    def subsets_cascading(self, nums: List[int]) -> List[List[int]]:
+        output = [[]]
+        
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        
+        return output
+    
+    def subsets_backtracking(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0, curr = []):
+            output.append(curr[:])
+            for i in range(first, n):
+                curr.append(nums[i])
+                backtrack(i + 1, curr)
+                curr.pop()
+        
+        output = []
+        n = len(nums)
+        backtrack()
+        return output
--- backtracking/tests/test_subsets.py
+++ backtracking/tests/test_subsets.py
@@ -12,3 +12,14 @@ class SubsetsTestCase(unittest.TestCase):
         res = set(tuple(x) for x in self.sol.subsets([1,2,3]))
         answer = set(tuple(x) for x in [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
         assert res == answer
+    
+    def test_subsets_cascading(self):
+        res = set(tuple(x) for x in self.sol.subsets_cascading([1,2,3]))
+        answer = set(tuple(x) for x in [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
+        assert res == answer
+
+    def test_subsets_backtracking(self):
+        res = set(tuple(x) for x in self.sol.subsets_backtracking([1,2,3]))
+        answer = set(tuple(x) for x in [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
+        assert res == answer
+        
